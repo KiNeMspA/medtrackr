da93b95c98d31158b7f1c5200e3a67e7bad00f88
KiNeMspA <antonycraig999@gmail.com>
2025-05-26 19:54:21 +1000

    Fixed ProviderNotFoundException in HomeScreen by adding ChangeNotifierProvider in main.dart (commit c05f5da98c57ceb7b6ea6c24eb681648049fe6f5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:medtrackr/models/medication.dart';
+import 'package:medtrackr/providers/data_provider.dart';
 import 'package:medtrackr/screens/home_screen.dart';
 import 'package:medtrackr/screens/medication_form_screen.dart';
-import 'package:medtrackr/screens/history_screen.dart';
-import 'package:medtrackr/screens/settings_screen.dart';
 import 'package:medtrackr/screens/add_dosage_screen.dart';
 import 'package:medtrackr/screens/add_schedule_screen.dart';
 import 'package:medtrackr/screens/medication_details_screen.dart';
-import 'package:medtrackr/models/medication.dart';
-import 'package:medtrackr/models/dosage.dart';
+import 'package:medtrackr/screens/reconstitution_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:medtrackr/providers/data_provider.dart';
-import 'package:medtrackr/services/notification_service.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final notificationService = NotificationService();
-  await notificationService.init();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => DataProvider(notificationService: notificationService)),
-        Provider(create: (context) => notificationService),
-      ],
-      child: const MedTrackrApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
-class MedTrackrApp extends StatelessWidget {
-  const MedTrackrApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: const Color(0xFFFFC107),
-        scaffoldBackgroundColor: Colors.grey[200],
-        textTheme: const TextTheme(
-          headlineMedium: TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.bold, color: Colors.black),
-          bodyMedium: TextStyle(fontFamily: 'Roboto', color: Colors.grey),
-          bodyLarge: TextStyle(fontFamily: 'Roboto', fontSize: 18, color: Colors.black),
-          titleLarge: TextStyle(fontFamily: 'Roboto', fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+    return ChangeNotifierProvider(
+      create: (context) => DataProvider(),
+      child: MaterialApp(
+        title: 'MedTrackr',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.grey[200],
         ),
-        inputDecorationTheme: const InputDecorationTheme(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Color(0xFFFFC107)),
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => const HomeScreen(),
+          '/medication_form': (context) => const MedicationFormScreen(),
+          '/add_dosage': (context) => AddDosageScreen(
+            medication: ModalRoute.of(context)!.settings.arguments as Medication,
           ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
+          '/add_schedule': (context) => const AddScheduleScreen(),
+          '/medication_details': (context) => const MedicationDetailsScreen(),
+          '/reconstitute': (context) => ReconstitutionScreen(
+            medication: ModalRoute.of(context)!.settings.arguments as Medication,
           ),
-          filled: true,
-          fillColor: Colors.white,
-        ),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
-      routes: {
-        '/medication_form': (context) => MedicationFormScreen(
-          medication: ModalRoute.of(context)!.settings.arguments as Medication?,
-        ),
-        '/add_dosage': (context) {
-          final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
-          return AddDosageScreen(
-            medication: args['medication'] as Medication,
-            dosage: args['dosage'] as Dosage?,
-          );
+          '/calendar': (context) => const Placeholder(),
+          '/history': (context) => const Placeholder(),
+          '/settings': (context) => const Placeholder(),
         },
-        '/add_schedule': (context) => const AddScheduleScreen(),
-        '/medication_details': (context) => MedicationDetailsScreen(
-          medication: ModalRoute.of(context)!.settings.arguments as Medication,
-        ),
-      },
-    );
-  }
-}
-
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (_selectedIndex != 0) {
-          setState(() {
-            _selectedIndex = 0;
-          });
-          return false;
-        }
-        return true;
-      },
-      child: Scaffold(
-        body: [
-          const HomeScreen(),
-          const MedicationFormScreen(),
-          const HistoryScreen(),
-          const SettingsScreen(),
-        ][_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.add_circle), label: 'Add'),
-            BottomNavigationBarItem(icon: Icon(Icons.history), label: 'History'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: const Color(0xFFFFC107),
-          unselectedItemColor: Colors.grey,
-          backgroundColor: Colors.white,
-          elevation: 8,
-          onTap: _onItemTapped,
-        ),
       ),
     );
   }
